125b4168222d8b6cbc6055492c30d92e87dc94b5
holewa <72799599+holewa@users.noreply.github.com>
2021-05-12 14:01:18 +0200

    hej

--- src/components/Sidemenu.js
+++ src/components/Sidemenu.js
@@ -1,33 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Sidemenu({ setTrigger }) {
   const handleClose = () => {
     setTrigger(false);
   };
+
   return (
     <div className="sidemenu">
-      <i className="ri-close-line ri-3x" onClick={handleClose}></i>
+      <Link>
+        <i
+          className="ri-close-line ri-3x"
+          style={{ marginLeft: '.5em' }}
+          onClick={handleClose}
+        ></i>
+      </Link>
       <ul>
-        <li>
-          <i className="ri-home-2-fill ri-3x" />
-          Hem
-        </li>
-        <li>
-          <img src="Blogo.svg" />
-          Om oss
-        </li>
-        <li>
-          <i className="ri-phone-fill ri-3x" />
-          Kontakt
-        </li>
-        <li>
-          <i className="ri-key-fill ri-3x" />
-          Logga in
-        </li>
-        <li>
-          <img src="TryBooksteplogo.svg" />
-          Prova Bookstep
-        </li>
+        <Link to="/">
+          <li>
+            <i className="ri-home-2-fill ri-3x" />
+            Hem
+          </li>
+        </Link>
+        <Link>
+          <li>
+            <img
+              src="Blogo.svg"
+              style={{ width: '15%', height: '15%', marginTop: '1em' }}
+            />
+            Om oss
+          </li>
+        </Link>
+        <Link>
+          <li>
+            <i className="ri-phone-fill ri-3x" />
+            Kontakt
+          </li>
+        </Link>
+        <Link>
+          <li>
+            <i className="ri-key-fill ri-3x" />
+            Logga in
+          </li>
+        </Link>
+        <Link>
+          <li>
+            <img
+              src="TryBooksteplogo.svg"
+              style={{ width: '15%', height: '15%' }}
+            />
+            Prova Bookstep
+          </li>
+        </Link>
       </ul>
     </div>
   );
